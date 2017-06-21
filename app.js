a737277ddf0c9720fcfc5a7f568d013758e8e336
Grant Paschal <gpass94@gmail.com>
2017-06-21 16:08:40 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const mustacheExpress = require("mustache-express");
+const bodyParser = require("body-parser");
+const expressValidator = require("express-validator");
+const app = express();
+
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(expressValidator());
+app.use(express.static("public"));
+
+app.engine("mustache", mustacheExpress());
+app.set("views", "./views");
+app.set("view engine", "mustache");
+
+
+const todos = [
+  "Wash the car",
+];
+
+let completed =[]
+app.get("/", function (req, res) {
+  res.render('index', { "todos": todos });
+});
+
+app.post("/", function (req, res) {
+  todos.push(req.body.todo);
+  res.redirect('/');
+});
+
+
+app.listen(3000, function () {
+  console.log("App is running on localhost 3000");
+});
